dc8d021822de6687a725d2968fa7103960a81fa1
eregandev <120528015+eregandev@users.noreply.github.com>
2022-12-22 18:12:18 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-new
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-2jhsdu)
---
+++ index.js
@@ -0,0 +1,14 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+let rating = 3;
+
+if(rating === 3);
+{
+  console.log('AMAZING');
+  
+}
